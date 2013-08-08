3aa521b12bb7b76af4cc3b3850a6088b02bce96a
PMByrnes <patrick.m.byrnes@gmail.com>
2013-08-07 21:02:53 -0400

    dev-sprint1

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,12 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Pat Byrnes (PMByrnes)
+
+def is_triangle(a, b, c):
+	if (a+b) <= c:
+		print 'Not a Triangle'
+	elif (a+c) <= b:
+		print 'Not a Triangle'
+	elif (b+c) <= a:
+		print 'Not a Triangle'
+	else:
+		print 'This is a Triangle'
